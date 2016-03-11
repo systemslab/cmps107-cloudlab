de2a1b7beb7006fa4a4dc09f8c8e8fb6c6c897c0
Andrew Shewmaker <agshew@gmail.com>
2016-03-11 15:58:50 -0800

    print percentiles while plotting

--- silly/silly-plots.R
+++ silly/silly-plots.R
@@ -22,10 +22,16 @@ if (n >= 1) {
   d1$op = "silly"
   xmin <- min(d1$Time)
   xmax <- max(d1$Time)
-  xmax999th <- quantile(d1$Time, c(0.999))
+  x50th <- round(quantile(d1$Time, c(0.50)), digits=3)
+  x90th <- round(quantile(d1$Time, c(0.90)), digits=3)
+  x99th <- round(quantile(d1$Time, c(0.99)), digits=3)
+  x999th <- round(quantile(d1$Time, c(0.999)), digits=3)
   d <- d1
   c1 <- d1
 }
+
+cat(paste("experiment", "xmin", "x50th", "x90th", "x99th", "x999th", "xmax", sep='\t'), '\n')
+cat(paste("before    ", xmin, x50th, x90th, x99th, x999th, xmax, sep='\t'), '\n')
 
 if (n >= 2) {
   d2 <- read.table(argv[2], header=F, col.names=columns)
@@ -33,10 +39,15 @@ if (n >= 2) {
   d2$op = "silly"
   xmin <- min(xmin, d2$Time)
   xmax <- max(xmax, d2$Time)
-  xmax999th <- max(xmax999th, quantile(d2$Time, c(0.999)), na.rm=TRUE)
+  x50th <- round(quantile(d1$Time, c(0.50)), digits=3)
+  x90th <- round(quantile(d1$Time, c(0.90)), digits=3)
+  x99th <- round(quantile(d1$Time, c(0.99)), digits=3)
+  x999th <- round(max(x999th, quantile(d2$Time, c(0.999)), na.rm=TRUE), digits=3)
   d <- rbind(d1, d2)
   c2 <- d2
 }
+
+cat(paste("after     ", xmin, x50th, x90th, x99th, x999th, xmax, sep='\t'), '\n')
 
 library(ggplot2)
 
@@ -46,7 +57,7 @@ xlabel <- expression(paste("Log of Latency (jiffies)"))
 p <- ggplot(d, aes(Time, colour=type))
 p + stat_ecdf(size=2) +
     ylab("Empirical CDF") +
-    scale_x_log10(xlabel, limits=c(xmin, xmax999th))
+    scale_x_log10(xlabel, limits=c(xmin, x999th))
 
 dev.off()
 
@@ -57,6 +68,6 @@ p <- ggplot(d, aes(Time, fill=factor(type)))
 p + stat_density(size=2) +
     scale_fill_discrete(name = "Operation Type") +
     ylab("Empirical Density") +
-    scale_x_log10(xlabel, limits=c(xmin, xmax999th))
+    scale_x_log10(xlabel, limits=c(xmin, x999th))
 
 dev.off()
